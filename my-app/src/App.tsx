652c1bcdc545a107738ecb09aba65f48afd019a4
RoeiBason <roeibason1234@gmail.com>
2025-06-22 18:01:47 +0300

    added logic for monkey

---
+++ my-app/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Monkey from './Features/Monkey';
+
+function App() {
+  return (
+    <div className="App">
+      <Monkey></Monkey>
+    </div>
+  );
+}
+
+export default App;
